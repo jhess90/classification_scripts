5e227f96c4349456d3422769828ac2bd45cc8360
jhess90 <jhess90@gmail.com>
2016-06-09 13:10:47 -0400

    modified npy to xlx converter

--- read_classifier_npy.py
+++ read_classifier_npy.py
@@ -10,26 +10,32 @@ data = np.load('accuracy_report_all.npy')[()]
 workbook = xlsxwriter.Workbook('data.xlsx')
 worksheet = workbook.add_worksheet()
 
-#d = {'a':['e1','e2','e3'],'b':['e1','e2'],'c':['e1']}
 row = 0
 col = 0
+
+worksheet.write(row,col,'Time')
+worksheet.write(row,col+1,'Region')
+worksheet.write(row,col+2,'Classification type')
+worksheet.write(row,col+3,'SAMME')
+worksheet.write(row,col+4,'SAMME.R')
 
 for key in data.keys():
     row += 1
     worksheet.write(row,col,key)
     #row += 1
     for item in data[key].keys():
-        row +=1
+        #row +=1
         worksheet.write(row,col + 1,item)
         #row += 1
-        for item2 in data[key].keys():
-            row +=1
+        for item2 in data[key][item].keys():
+            #row +=1
             worksheet.write(row,col+2,item2)
             #row += 1
-            for item3 in data[key][item2].keys():
-                row +=1
-                worksheet.write(row,col+3,item3)
-                #row += 1
-			
+            #for item3 in data[key][item][item2].keys():
+			#row +=1
+            worksheet.write(row,col+3,data[key][item][item2]['accuracy_samme'])
+            worksheet.write(row,col+4,data[key][item][item2]['accuracy_sammer'])
+            row += 1
+            
 workbook.close()
 
