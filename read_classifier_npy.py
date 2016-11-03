4c0c925b5f1c0317f2282ea1168e89098b4d8e1c
jhess90 <jhess90@gmail.com>
2016-11-03 14:50:00 -0400

    working on muliple levels classification, some errors. committing to revert to old rp_classifier.py and see what's going on

--- read_classifier_npy.py
+++ read_classifier_npy.py
@@ -16,25 +16,24 @@ col = 0
 worksheet.write(row,col,'Time')
 worksheet.write(row,col+1,'Region')
 worksheet.write(row,col+2,'Classification type')
-worksheet.write(row,col+3,'SAMME')
-worksheet.write(row,col+4,'SAMME.R')
+worksheet.write(row,col+3,'SAMME avg')
+worksheet.write(row,col+4,'SAMME std')
+worksheet.write(row,col+5,'SAMME.R avg')
+worksheet.write(row,col+6,'SAMME.R std')
 
 for key in data.keys():
     row += 1
     worksheet.write(row,col,key)
-    #row += 1
     for item in data[key].keys():
-        #row +=1
         worksheet.write(row,col + 1,item)
-        #row += 1
         for item2 in data[key][item].keys():
-            #row +=1
             worksheet.write(row,col+2,item2)
-            #row += 1
-            #for item3 in data[key][item][item2].keys():
-			#row +=1
-            worksheet.write(row,col+3,data[key][item][item2]['accuracy_samme'])
-            worksheet.write(row,col+4,data[key][item][item2]['accuracy_sammer'])
+            #worksheet.write(row,col+3,data[key][item][item2]['accuracy_samme'])
+            #worksheet.write(row,col+4,data[key][item][item2]['accuracy_sammer'])
+            worksheet.write(row,col+3,data[key][item][item2]['accuracy_samme_avg'])
+            worksheet.write(row,col+4,data[key][item][item2]['accuracy_samme_std'])
+            worksheet.write(row,col+5,data[key][item][item2]['accuracy_sammer_avg'])
+            worksheet.write(row,col+6,data[key][item][item2]['accuracy_sammer_std'])
             row += 1
             
 workbook.close()
