61e1025e66d0b97650e086a135bb7b1d1a655993
jhess90 <jhess90@gmail.com>
2017-01-02 12:23:35 -0500

    plotting working, saving average and nlized firing rates for different event types for each unit

--- plot_rasters.py
+++ plot_rasters.py
@@ -30,7 +30,7 @@ from sklearn.tree import DecisionTreeClassifier
 plot_bool = True
 time_boundry={'-0.5-1.0':[-0.5,1.0]}
 
-bin_size = 500 #in ms
+bin_size = 50 #in ms
 
 #### laptop ##############
 
@@ -245,21 +245,17 @@ PmV_spikes = np.array(dummy);
 print("The number of units in PmV is: %s"%(len(PmV_spikes)))
 unit_names['pmd_unit_names']=pmd_unit_names
 
-#print ('%s bins (50 ms each)\n%s pca components\n2 LDA components' %(no_bins,pca_num))
-
 #trim M1,S1,and PmD spikes to remove PmV spikes trailing at end; nerual data pruning now complete
 M1_spikes = M1_spikes[0,0:M1_limit];
 S1_spikes = S1_spikes[0,0:S1_limit];
 PmD_spikes = PmD_spikes[0,0:PmD_limit];
-#generate time intervals of interest for PSTH generation
 
+#generate time intervals of interest for PSTH generation
 #time_boundry={'-0.5-0.0':[-0.5,0.0],'0-0.5':[0.0,0.5], '0.5-1.0':[0.5,1.0], '1.0-1.5':[1.0,1.5], '1.5-2.0':[1.5,2.0],'all':[-0.5,2.0]}
 #time_boundry={'-0.5-0.0':[-0.5,0.0],'0-0.5':[0.0,0.5],'0.5-1.0':[0.5,1.0],'all_incpre':[-0.5,2.0]}
 #time_boundry={'-0.5-0.0':[-0.5,0.0],'0-0.5':[0.0,0.5],'0.5-1.0':[0.5,1.0],'all':[0.0,2.0]}
 
 
-#accuracy_total = {}
-all_accuracy_total = {}
 #Begin main loop of analysis
 for name_of_bin,time_of_bin in time_boundry.iteritems():
     before_time = -time_of_bin[0]
@@ -308,52 +304,72 @@ for name_of_bin,time_of_bin in time_boundry.iteritems():
             dummy.append(np.hstack(data[start_position+56][i]))
         p_only_f_data = np.array(dummy)
         p_only_f_targets = np.ones(p_only_f_data.shape[0])*7
-
-
 		
-#        return (np.vstack([rp_s_data,rp_f_data,nrnp_s_data,nrnp_f_data,r_only_s_data,r_only_f_data,p_only_s_data,p_only_f_data]),np.hstack([rp_s_targets,rp_f_targets,nrnp_s_targets,nrnp_f_targets,r_only_s_targets,r_only_f_targets,p_only_s_targets,p_only_f_targets]))
         return(np.vstack([rp_s_data,rp_f_data,nrnp_s_data,nrnp_f_data,r_only_s_data,r_only_f_data,p_only_s_data,p_only_f_data]),np.hstack([rp_s_targets,rp_f_targets,nrnp_s_targets,nrnp_f_targets,r_only_s_targets,r_only_f_targets,p_only_s_targets,p_only_f_targets]))
 
 
-#rp_s_rdelivery
 time_before = -0.5 #make sure if change to pos time before it still works
 time_after = 1.0
 
 no_bins = (time_after - time_before) * 1000 / bin_size
 
+def normalize_data(spike_data):
+	hist = []
+	nl_spike_data = []
+	hist_all = []
+	nl_dict = {}
+	min_bin = np.zeros(len(spike_data))
+	max_bin = np.zeros(len(spike_data))
+	
+	for i in range(len(spike_data)):
+		total_bin_range = np.arange(0,int(np.ceil(spike_data[i].max())),bin_size/1000.0)
+		hist = np.histogram(spike_data[i], bins=total_bin_range,range=(0,int(np.ceil(spike_data[i].max()))),normed=False, density=False)
+
+		min_bin[i] = min(hist[0])
+		max_bin[i] = max(hist[0])
+		
+		#num = np.subtract(hist[0],min(hist[0]))
+		#denom = max(hist[0]) - min(hist[0])
+		num = np.subtract(hist[0],min_bin[i])
+		denom = max_bin[i]-min_bin[i]
+		
+		nl_spike_data.append(np.true_divide(num,denom))
+		hist_all.append(hist)
+
+	nl_dict['hist_all'] = hist_all
+	nl_dict['nl_spike_data'] = nl_spike_data
+	nl_dict['min_bin'] = min_bin
+	nl_dict['max_bin'] = max_bin
+	
+	
+	return(nl_dict)
+	
+
 def make_raster(data,spikes,key,key2,unit_no,time_before,time_after):
 
 	dummy = []
+	
 	for i in range(0,data.shape[0]):
 		dummy1=[]
 		b = data[i]
 		for j in range(len(spikes)):
-			#a = spikes[j]['ts'][0,0][0]
 			a = spikes[j]
 			c = a[np.where(np.logical_and(a>=b+time_before,a<=b+time_after))]
 			dummy1.append(c)
-			#dummy1.append(np.nan_to_num(c[0]))
 		dummy.append(dummy1)
 
 	dummy = np.asarray(dummy)
-
 	ax = plt.gca()
-
 	adjusted_times = []
 	binned = []
-	#firing_rate = []
 
-	#fig,axs = plt.subplots(nrows=1, ncols=2, sharex=True)
+	#rasters
 	plt.subplot(2,1,1)
-	
-	#ax=axs[0,0]
 	
 	for i in range(dummy.shape[0]):
 		adjusted_times=dummy[i][unit_no]-value[i]
 		plt.vlines(adjusted_times, i+.5, i+1.5)
 		plt.ylim(.5, dummy.shape[0] + .5)
-
-		#TODO normalize here
 
 		binned_temp = np.histogram(adjusted_times,bins=no_bins,normed=False,density=False)
 		binned.append(np.nan_to_num(binned_temp[0]))
@@ -361,34 +377,25 @@ def make_raster(data,spikes,key,key2,unit_no,time_before,time_after):
 	binned = np.asarray(binned)
 	firing_rate = np.zeros(len(binned),dtype=object)
 	for i in range(len(binned)):
-		firing_rate[i] = binned[i] / float(bin_size) * 1000 #because bin_size in ms, want to get Hz
+ 		firing_rate[i] = binned[i] / float(bin_size) * 1000 #because bin_size in ms, want to get Hz
 
 	avg_firing_rate = np.mean(firing_rate)
 	std_dev_firing_rate = np.std(firing_rate)
 	
-	
 	plt.axvline(0.0)
 	
-	#plt.title('Ex raster plot,%s, %s, unit: %s' %(key,key2,str(unit_no)))
 	plt.title('Raster plot')
-	#ax.xlabel('Time')
-	#ax.ylabel('Trial')
-	#plt.show()
+	plt.xlabel('Time from event')
+	plt.ylabel('Trial')
 
-	#ax=axs[0,1]
+	#average firing rate
 	plt.subplot(2,1,2)
-	#plt.plot(avg_firing_rate)
-	#plt.plot((avg_firing_rate + std_dev_firing_rate))
-	#plt.plot(avg_firing_rate - std_dev_firing_rate)
-	#plt.plot(avg_firing_rate)
-	#plt.xlim(0,30)
-
-	#plt.errorbar(avg_firing_rate,std_dev_firing_rate) #,yerr=yerr,fmt=o)
 	plt.title('average firing rate')
 
-	#plt.fill_between(avg_firing_rate,std_dev_firing_rate,std_dev_firing_rate,alpha=1, edgecolor='#3F7F4C', facecolor='#7EFF99',linewidth=0)
-	#plt.fill_between(avg_firing_rate,avg_firing_rate-10,avg_firing_rate+10)   #,alpha=1, edgecolor='#3F7F4C', facecolor='#7EFF99',linewidth=0)
-
+	#TODO make time instead of bin
+	plt.xlabel('Bin num')
+	plt.ylabel('Firing rate (Hz)')
+	
 	ymax = avg_firing_rate + std_dev_firing_rate
 	ymin = avg_firing_rate - std_dev_firing_rate
 
@@ -397,42 +404,202 @@ def make_raster(data,spikes,key,key2,unit_no,time_before,time_after):
 	plt.errorbar(np.arange(no_bins),avg_firing_rate,yerr=std_dev_firing_rate)
 	plt.xlim(0,no_bins)
 	plt.ylim(ymin=0)
+
+	plt.axvline(0.0)
 	
-	#fig.suptitle('%s, %s, unit: %s' %(key,key2,str(unit_no)))
-	plt.suptitle('%s, %s, unit: %s' %(key,key2,str(unit_no)))
+	plt.suptitle('%s, %s, unit: %s' %(key,key2,str(unit_no)), fontsize = 20)
+
+	plt.tight_layout()
+	plt.subplots_adjust(top=0.85)
 	
 	plt.savefig('raster_%s_%s_unit%s' %(key, key2, str(unit_no)))
 	plt.clf()
 
-	return_dict={'avg_firing_rate':avg_firing_rate,'std_dev_firing_rate':std_dev_firing_rate,'binned':binned,'firing_rate':firing_rate,'adjusted_times':adjusted_times}
+	return_dict={'avg_firing_rate':avg_firing_rate,'std_dev_firing_rate':std_dev_firing_rate,'binned':binned,'firing_rate':firing_rate,'adjusted_times':adjusted_times,'dummy':dummy}
 	
 	return(return_dict)
 
-#unit_no for loop
-#unit_no = 20
+
+
+def make_nl_raster(data,spikes,nl,key,key2,unit_no,time_before,time_after,min_bin,max_bin):
+
+	dummy = []
+
+	for i in range(len(data)):
+		dummy1=[]
+		b = data[i]
+		for j in range(len(spikes)):
+			a = spikes[j]
+			c = a[np.where(np.logical_and(a >= b+time_before, a<= b+time_after))]
+			dummy1.append(c)
+		dummy.append(dummy1)
+
+	dummy = np.asarray(dummy)
+
+	ax = plt.gca()
+	adjusted_times = []
+	binned = []
+	binned_nl = []
+	plt.subplot(2,2,1)
+
+	for i in range(dummy.shape[0]):
+		adjusted_times = dummy[i][unit_no] - data[i]
+		plt.vlines(adjusted_times, i+.5, i+1.5)
+		plt.ylim(.5, dummy.shape[0] + .5)
+
+		binned_temp = np.histogram(adjusted_times,bins=no_bins,normed=False,density=False)
+		binned.append(np.nan_to_num(binned_temp[0]))
+
+		binned_nl_temp_num = binned_temp[0] - min_bin[i]
+		binned_nl_temp_denom = max_bin[i] - min_bin[i]
+
+		binned_nl_temp = np.divide(binned_nl_temp_num,binned_nl_temp_denom)
+		binned_nl.append(binned_nl_temp)
+	
+	binned = np.asarray(binned)
+	binned_nl = np.asarray(binned_nl)
+	firing_rate = np.zeros(len(binned),dtype=object)
+	for i in range(len(binned)):
+ 		firing_rate[i] = binned[i] / float(bin_size) * 1000 #because bin_size in ms, want to get Hz
+
+	avg_firing_rate = np.mean(firing_rate)
+	std_dev_firing_rate = np.std(firing_rate)
+
+	avg_nl = np.mean(binned_nl,axis=0)
+	std_dev_nl = np.std(binned_nl,axis=0)
+	
+	plt.axvline(0.0,color='r')
+	plt.xlim(time_before,time_after)
+	
+	plt.title('Raster plot')
+	plt.xlabel('Time from event')
+	plt.ylabel('Trial')
+
+	#Average firing rate
+	plt.subplot(2,2,2)
+	plt.title('average firing rate')
+	plt.xlabel('Time from event')
+	plt.ylabel('Firing rate (Hz)')
+	
+	ymax = avg_firing_rate + std_dev_firing_rate
+	ymin = avg_firing_rate - std_dev_firing_rate
+
+	#plt.plot(avg_firing_rate)
+	#plt.fill_between(ymin,ymax,alpha=1,edgecolor='#3F7F4C',facecolor='#7EFF99',linewidth=0)
+	plt.errorbar(np.linspace(time_before,time_after,no_bins,endpoint=True),avg_firing_rate,yerr=std_dev_firing_rate)
+	plt.ylim(ymin=0)
+	plt.xlim(time_before,time_after)
+	plt.axvline(0.0,color='r')
+
+	#normalized firing rate
+	plt.subplot(2,2,3)
+
+	plt.errorbar(np.linspace(time_before,time_after,no_bins,endpoint=True),avg_nl,yerr=std_dev_nl)
+	plt.ylim(0,1.0)
+	plt.xlim(time_before,time_after)
+	plt.axvline(0.0,color='r')
+	
+	plt.title('average normalized firing rate')
+	plt.xlabel('Time from event')
+	plt.ylabel('Normalized firing rate')
+
+	plt.suptitle('%s, %s, unit: %s' %(key,key2,str(unit_no)), fontsize = 20)
+	plt.tight_layout()
+	plt.subplots_adjust(top=0.85)
+	
+	plt.savefig('raster_%s_%s_unit%s' %(key, key2, str(unit_no)))
+	plt.clf()
+
+	#return_dict={'avg_firing_rate':avg_firing_rate,'std_dev_firing_rate':std_dev_firing_rate,'binned':binned,'firing_rate':firing_rate,'adjusted_times':adjusted_times,'dummy':dummy,'spikes':spikes,'dummy1':dummy1,'a':a,'b':b,'c':c,'nlized':nlized,'binned_temp':binned_temp,'binned_nl':binned_nl} #'a_nl':a_nl,'b_nl':b_nl,'c_nl':c_nl,'nlized':nlized,'dummy_nl':dummy_nl}
+	return_dict={'avg_firing_rate':avg_firing_rate,'std_dev_firing_rate':std_dev_firing_rate,'adjusted_times':adjusted_times,'binned':binned,'binned_nl':binned_nl,'firing_rate':firing_rate,'avg_nl':avg_nl,'std_dev_nl':std_dev_nl}
+	
+	return(return_dict)
+
 
 ts_dict={'rp_s_cue':rp_s_cue, 'rp_s_rdelivery':rp_s_rdelivery, 'rp_f_cue':rp_f_cue, 'rp_f_pdelivery':rp_f_pdelivery, 'nrnp_s_cue':nrnp_s_cue, 'nrnp_s_nextreset':nrnp_s_nextreset, 'nrnp_f_cue':nrnp_f_cue, 'nrnp_f_nextreset':nrnp_f_nextreset, 'r_only_s_cue':r_only_s_cue, 'r_only_s_rdelivery':r_only_s_rdelivery, 'r_only_f_cue':r_only_f_cue, 'r_only_f_nextreset':r_only_f_nextreset, 'p_only_s_cue':p_only_s_cue, 'p_only_s_nextreset':p_only_s_nextreset, 'p_only_f_cue':p_only_f_cue, 'p_only_f_pdelivery':p_only_f_pdelivery}
 
 #testing dict
 #ts_dict={'rp_s_cue':rp_s_cue, 'r_only_s':r_only_s_rdelivery,'nrnp_s_cue':nrnp_s_cue}
-#ts_dict={'r_only_f_nextreset':r_only_f_nextreset}
 #ts_dict = {'rp_s_rdelivery':rp_s_rdelivery}
 
 data_dict={'M1_spikes':M1_spikes,'S1_spikes':S1_spikes,'PmD_spikes':PmD_spikes,'PmV_spikes':PmV_spikes}
 
+
+data_dict_nl = {}
+for key, value in data_dict.iteritems():
+	spike_data = []
+	if key == 'PmV_spikes':
+		for i in range(len(value)):
+			spike_data.append(value[i])
+	else:
+		for i in range(len(value)):
+			spike_data.append(value[i]['ts'][0,0][0])
+
+	print 'normalizing %s' %(key)		
+	normalized_data = normalize_data(spike_data)
+	data_dict_nl['%s_nl' %(key)] = normalized_data
+
+	
+M1_dicts = {'spikes':data_dict['M1_spikes'],'nl':data_dict_nl['M1_spikes_nl']} 
+S1_dicts = {'spikes':data_dict['S1_spikes'],'nl':data_dict_nl['S1_spikes_nl']}
+PmD_dicts = {'spikes':data_dict['PmD_spikes'],'nl':data_dict_nl['PmD_spikes_nl']}
+PmV_dicts = {'spikes':data_dict['PmV_spikes'],'nl':data_dict_nl['PmV_spikes_nl']}
+
+data_dict_all = {'M1_dicts':M1_dicts,'S1_dicts':S1_dicts,'PmD_dicts':PmD_dicts,'PmV_dicts':PmV_dicts}
+
+save_dict = {}
+save_dict['data_dict_all'] = data_dict_all
+
+#total_rasters = []
+#for key, value in ts_dict.iteritems():
+#	if not (len(value) == 1 and value[0] == 0):
+#		for key2, value2 in data_dict.iteritems():
+#		#for key2, value2 in data_dict_nl.iteritems():
+#			print 'making rasters %s, %s' %(key, key2)
+#			spikes=[]
+#
+#			if key2 == 'PmV_spikes':
+#				for i in range(len(value2)):
+#					spikes.append(value2[i])
+#			else:
+#				for i in range(len(value2)):
+#					spikes.append(value2[i]['ts'][0,0][0])			
+#				for i in range(len(spikes)):
+#					rasters = make_raster(value,spikes,key,key2,i,time_before,time_after)
+#
+#			#for i in range(len(value2['nl_spike_data'])):
+#				#rasters = make_nl_raster(value,value2['nl_spike_data'],key,key2,i,time_before,time_after)
+#				#total_rasters.append(rasters)
+#			#save_dict['%s_%s' %(key, key2)] = total_rasters
+#	else:
+#		print 'no instances of event %s, no plot generation' %(key)
+
+
+
+total_rasters = []
 for key, value in ts_dict.iteritems():
 	if not (len(value) == 1 and value[0] == 0):
-		for key2, value2 in data_dict.iteritems():
+		for key2, value2 in data_dict_all.iteritems():
 			print 'making rasters %s, %s' %(key, key2)
-			spikes=[]
 
-			if key2 == 'PmV_spikes':
-				for i in range(len(value2)):
-					spikes.append(value2[i])
+			spikes=[]
+			if key2 == 'PmV_dicts':
+				for i in range(len(data_dict_all[key2]['spikes'])):
+					spikes.append(data_dict_all[key2]['spikes'][i])
 			else:
-				for i in range(len(value2)):
-					spikes.append(value2[i]['ts'][0,0][0])			
-				for i in range(len(spikes)):
-					rasters = make_raster(value,spikes,key,key2,i,time_before,time_after)
+				for i in range(len(data_dict_all[key2]['spikes'])):
+					spikes.append(data_dict_all[key2]['spikes'][i]['ts'][0,0][0])			
+
+			for i in range(len(data_dict_all[key2]['spikes'])):
+				rasters = make_nl_raster(value,spikes,data_dict_all[key2]['nl']['nl_spike_data'],key,key2,i,time_before,time_after,data_dict_all[key2]['nl']['min_bin'],data_dict_all[key2]['nl']['max_bin'])
+
+				total_rasters.append(rasters)
+			save_dict['%s_%s' %(key, key2)] = total_rasters
 	else:
 		print 'no instances of event %s, no plot generation' %(key)
+
+short_filename = filename[-27:-4]
+if short_filename.startswith('0'):
+	short_filename = '0%s' %(short_filename)
+
+np.save('avg_fr_and_nlized_data_%s' %(short_filename),save_dict)
