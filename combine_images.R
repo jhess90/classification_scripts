dd70d557fd4bb7575b42e4ca5289c8c5773e54ed
jhess90 <jhess90@gmail.com>
2017-07-07 18:16:35 -0400

    fixed typo

--- combine_images.R
+++ combine_images.R
@@ -40,23 +40,23 @@ for (i in 1:length(region_list)){
   region_key=region_list[i]
   
   name1 <- paste(region_key,"_all_r",sep="")
-  files1 <- c(paste(region_key,"_all_r_succ_cue.png",sep=""),paste(region_key,"_all_r_fail_cue.png",sep=""),"ra_succ_cue_gf.png","ra_fail_cue_gf.png",paste(region_key,"_all_r_succ_result.png",sep=""),paste(region_key,"_all_r_fail_result.png",sep=""),"ra_succ_result_gf.png","ra_fail_resul_gf.png")
+  files1 <- c(paste(region_key,"_all_r_succ_cue.png",sep=""),paste(region_key,"_all_r_fail_cue.png",sep=""),"ra_succ_cue_gf.png","ra_fail_cue_gf.png",paste(region_key,"_all_r_succ_result.png",sep=""),paste(region_key,"_all_r_fail_result.png",sep=""),"ra_succ_result_gf.png","ra_fail_result_gf.png")
   combine_fnct(name1,files1)
   
   name2 <- paste(region_key,"_all_p",sep="")
-  files2 <- c(paste(region_key,"_all_p_succ_cue.png",sep=""),paste(region_key,"_all_p_fail_cue.png",sep=""),"pa_succ_cue_gf.png","pa_fail_cue_gf.png",paste(region_key,"_all_p_succ_result.png",sep=""),paste(region_key,"_all_p_fail_result.png",sep=""),"pa_succ_result_gf.png","pa_fail_resul_gf.png")
+  files2 <- c(paste(region_key,"_all_p_succ_cue.png",sep=""),paste(region_key,"_all_p_fail_cue.png",sep=""),"pa_succ_cue_gf.png","pa_fail_cue_gf.png",paste(region_key,"_all_p_succ_result.png",sep=""),paste(region_key,"_all_p_fail_result.png",sep=""),"pa_succ_result_gf.png","pa_fail_result_gf.png")
   combine_fnct(name2,files2)
   
   name3 <- paste(region_key,"_no_r",sep="")
-  files3 <- c(paste(region_key,"_no_r_succ_cue.png",sep=""),paste(region_key,"_no_r_fail_cue.png",sep=""),"r0_succ_cue_gf.png","r0_fail_cue_gf.png",paste(region_key,"_no_r_succ_result.png",sep=""),paste(region_key,"_no_r_fail_result.png",sep=""),"r0_succ_result_gf.png","r0_fail_resul_gf.png")
+  files3 <- c(paste(region_key,"_no_r_succ_cue.png",sep=""),paste(region_key,"_no_r_fail_cue.png",sep=""),"r0_succ_cue_gf.png","r0_fail_cue_gf.png",paste(region_key,"_no_r_succ_result.png",sep=""),paste(region_key,"_no_r_fail_result.png",sep=""),"r0_succ_result_gf.png","r0_fail_result_gf.png")
   combine_fnct(name3,files3)
   
   name4 <- paste(region_key,"_no_p",sep="")
-  files4 <- c(paste(region_key,"_no_p_succ_cue.png",sep=""),paste(region_key,"_no_p_fail_cue.png",sep=""),"p0_succ_cue_gf.png","p0_fail_cue_gf.png",paste(region_key,"_no_p_succ_result.png",sep=""),paste(region_key,"_no_p_fail_result.png",sep=""),"p0_succ_result_gf.png","p0_fail_resul_gf.png")
+  files4 <- c(paste(region_key,"_no_p_succ_cue.png",sep=""),paste(region_key,"_no_p_fail_cue.png",sep=""),"p0_succ_cue_gf.png","p0_fail_cue_gf.png",paste(region_key,"_no_p_succ_result.png",sep=""),paste(region_key,"_no_p_fail_result.png",sep=""),"p0_succ_result_gf.png","p0_fail_result_gf.png")
   combine_fnct(name4,files4)
   
   name5 <- paste(region_key,"_catch",sep="")
-  files5 <- c(paste(region_key,"_r_all_catch_cue.png",sep=""),paste(region_key,"_p_all_catch_cue.png",sep=""),"r_all_catch_cue_gf.png","p_all_catch_cue_gf.png",paste(region_key,"_r_all_catch_result.png",sep=""),paste(region_key,"_p_all_catch_result.png",sep=""),"r_all_catch_result_gf.png","p_all_catch_resul_gf.png")
+  files5 <- c(paste(region_key,"_r_all_catch_cue.png",sep=""),paste(region_key,"_p_all_catch_cue.png",sep=""),"r_all_catch_cue_gf.png","p_all_catch_cue_gf.png",paste(region_key,"_r_all_catch_result.png",sep=""),paste(region_key,"_p_all_catch_result.png",sep=""),"r_all_catch_result_gf.png","p_all_catch_result_gf.png")
   combine_fnct(name5,files5)
   
 }
