4fd8643483521371ad1857bffa7c7c135fec18e7
jhess90 <jhess90@gmail.com>
2017-09-08 18:51:08 -0400

    adding gpfa arrangement and running scripts

---
+++ arrange_gpfa_data.m
@@ -0,0 +1,51 @@
+load('master_hist.mat');
+
+bfr_cue = M1_hist_dict.bfr_cue;
+aft_cue = M1_hist_dict.aft_cue;
+bfr_result = M1_hist_dict.bfr_result;
+aft_result = M1_hist_dict.aft_result;
+
+[trials,units,bfr_bins] = size(bfr_cue);
+[trials2,units2,aft_bins] = size(aft_cue);
+
+
+bc_struct = struct([]);
+ac_struct = struct([]);
+br_struct = struct([]);
+ar_struct = struct([]);
+c_struct = struct([]);
+r_struct = struct([]);
+a_struct = struct([]);
+
+for i=1:trials
+
+    bc_struct(i).trialId = i;
+    bc_struct(i).spikes = squeeze(bfr_cue(i,:,:));
+
+    ac_struct(i).trialId = i;
+    ac_struct(i).spikes = squeeze(aft_cue(i,:,:));
+
+    br_struct(i).trialId = i;
+    br_struct(i).spikes = squeeze(bfr_result(i,:,:));
+
+    ar_struct(i).trialId = i;
+    ar_struct(i).spikes = squeeze(aft_result(i,:,:));
+
+    c_struct(i).trialId = i;
+    c_struct(i).spikes = cat(2,squeeze(bfr_cue(i,:,:)),squeeze(aft_cue(i,:,:)));
+ 
+    r_struct(i).trialId = i;
+    r_struct(i).spikes = cat(2,squeeze(bfr_result(i,:,:)),squeeze(aft_result(i,:,:)));
+    
+    a_struct(i).trialId = i;
+    a_struct(i).spikes = cat(2,squeeze(bfr_cue(i,:,:)),squeeze(aft_cue(i,:,:)),squeeze(bfr_result(i,:,:)),squeeze(aft_result(i,:,:)));
+ 
+end
+
+rewarding = find(condensed(:,4) > 0 & condensed(:,6) == 1);
+punishing = find(condensed(:,5) > 0 & condensed(:,6) == -1);
+
+
+save('cue.mat','c_struct')
+save('result.mat','r_struct')
+save('all.mat','a_struct')
