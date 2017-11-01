cc90d266e8a84efbc5c3efa499c577b14541c707
Jack Hessburg <jhess90@gmail.com>
2017-10-31 21:43:59 -0400

    saving snowball data

---
+++ run_dpca_1031.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+cd /home/jack/Dropbox/dpca/new/all_alt_cue/0059/nod/
+python /home/jack/Dropbox/classification_scripts/dpca_setup_binaryrp_nod.py
+python /home/jack/Dropbox/classification_scripts/analyze_dpca_nod.py
+Rscript /home/jack/Dropbox/classification_scripts/dpca_plot_analysis_nod.R
+
+cd /home/jack/Dropbox/dpca/new/all_alt_cue/0059/mv/
+pwd
+python /home/jack/classification_scripts/dpca_setup_alt_multi_m.py
+python /home/jack/classification_scripts/analyze_dpca_m.py
+Rscript /home/jack/classification_scripts/dpca_plot_analysis_m.py
+
+python /home/jack/classification_scripts/dpca_setup_alt_multi_v.py
+python /home/jack/classification_scripts/analyze_dpca_v.py
+Rscript /home/jack/classification_scripts/dpca_plot_analysis_v.py
+
+cd /home/jack/Dropbox/dpca/new/all_alt_cue/504/mv/
+pwd
+python /home/jack/classification_scripts/dpca_setup_alt_multi_m.py
+python /home/jack/classification_scripts/analyze_dpca_m.py
+Rscript /home/jack/classification_scripts/dpca_plot_analysis_m.py
+
+python /home/jack/classification_scripts/dpca_setup_alt_multi_v.py
+python /home/jack/classification_scripts/analyze_dpca_v.py
+Rscript /home/jack/classification_scripts/dpca_plot_analysis_v.py
+
+
+
+
+cd /home/jack/Dropbox/dpca/new/cued/gaus30_bin10/0059/mv/
+pwd
+python /home/jack/classification_scripts/dpca_setup_multi_m.py
+python /home/jack/classification_scripts/analyze_dpca_m.py
+Rscript ~/classification_scripts/dpca_plot_analysis_m.R
+
+python /home/jack/classification_scripts/dpca_setup_multi_v.py
+python /home/jack/classification_scripts/analyze_dpca_v.py
+Rscript ~/classification_scripts/dpca_plot_analysis_v.R
+
+cd /home/jack/Dropbox/dpca/new/cued/gaus30_bin10/504/mv/
+pwd
+python /home/jack/classification_scripts/dpca_setup_multi_m.py
+python /home/jack/classification_scripts/analyze_dpca_m.py
+Rscript ~/classification_scripts/dpca_plot_analysis_m.R
+
+python /home/jack/classification_scripts/dpca_setup_multi_v.py
+python /home/jack/classification_scripts/analyze_dpca_v.py
+Rscript ~/classification_scripts/dpca_plot_analysis_v.R
+
+exec bash
+
+
--- run_dpca_long.sh
+++ run_dpca_long.sh
@@ -11,13 +11,19 @@ cd /home/jack/Dropbox/dpca/new/cued/gaus30_bin10/504/multi_nod/
 pwd
 python /home/jack/classification_scripts/dpca_setup_multirp_nod.py
 python /home/jack/classification_scripts/analyze_dpca_nod.py
-Rscript ~/classification_scripts/dpca_plot_analysisz_nod.R
+Rscript ~/classification_scripts/dpca_plot_analysis_nod.R
 
 cd /home/jack/Dropbox/dpca/new/cued/gaus30_bin10/504/multi_d/
 pwd
 python /home/jack/classification_scripts/dpca_setup_multirp.py
 python /home/jack/classification_scripts/analyze_dpca.py
 Rscript ~/classification_scripts/dpca_plot_analysis.R
+
+cd /home/jack/Dropbox/dpca/new/all_alt_cue/0059/nod/
+pwd
+python /home/jack/classification_scripts/dpca_setup_binaryrp_nod.py
+python /home/jack/classfiication_scripts/analyze_dpca_nod.py
+Rscript ~/classficiation_scripts/dpca_plot_analysis_nod.R
 
 
 """
