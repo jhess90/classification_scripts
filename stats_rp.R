d3bb81e5f559adb4e622fa885a16663916264c28
jhess90 <jhess90@gmail.com>
2018-03-21 12:34:06 -0400

    test rp stats

--- stats_rp.R
+++ stats_rp.R
@@ -43,6 +43,8 @@ for(region_index in 1:length(region_list)){
   old_time <- seq(from=-0.5,to=(1.0-bin_size/1000),by=bin_size/1000)
   time <- seq(from=-0.5+2*bin_size/1000,to=(1.0-3*bin_size/1000),by=bin_size/1000)
   
+  
+  
   r0 <- which(condensed[,4] == 0)
   r1 <- which(condensed[,4] == 1)
   r2 <- which(condensed[,4] == 2)
@@ -73,18 +75,98 @@ for(region_index in 1:length(region_list)){
   res1 <- which(condensed[,6] == 1)
   
   r0_fail <- res0[which(res0 %in% r0)]
+  r1_fail <- res0[which(res0 %in% r1)]
+  r2_fail <- res0[which(res0 %in% r2)]
   r3_fail <- res0[which(res0 %in% r3)]
   r0_succ <- res1[which(res1 %in% r0)]
   r1_succ <- res1[which(res1 %in% r1)]
   r2_succ <- res1[which(res1 %in% r2)]
   r3_succ <- res1[which(res1 %in% r3)]
   
+  p0_fail <- res0[which(res0 %in% p0)]
+  p1_fail <- res0[which(res0 %in% p1)]
+  p2_fail <- res0[which(res0 %in% p2)]
+  p3_fail <- res0[which(res0 %in% p3)]
+  p0_succ <- res1[which(res1 %in% p0)]
+  p1_succ <- res1[which(res1 %in% p1)]
+  p2_succ <- res1[which(res1 %in% p2)]
+  p3_succ <- res1[which(res1 %in% p3)]
   
+  ###########
+  r0 <- which(condensed[,4] == 0)
+  r1 <- which(condensed[,4] == 1)
+  r2 <- which(condensed[,4] == 2)
+  r3 <- which(condensed[,4] == 3)
+  rx <- which(condensed[,4] >= 1)
   
+  p0 <- which(condensed[,5] == 0)
+  p1 <- which(condensed[,5] == 1)
+  p2 <- which(condensed[,5] == 2)
+  p3 <- which(condensed[,5] == 3)
+  px <- which(condensed[,5] >= 1)
+  
+  v_3 <- which(condensed[,7] == -3)
+  v_2 <- which(condensed[,7] == -2)
+  v_1 <- which(condensed[,7] == -1)
+  v0 <- which(condensed[,7] == 0)
+  v1 <- which(condensed[,7] == 1)
+  v2 <- which(condensed[,7] == 2)
+  v3 <- which(condensed[,7] == 3)
+  
+  m0 <- which(condensed[,8] == 0)
+  m1 <- which(condensed[,8] == 1)
+  m2 <- which(condensed[,8] == 2)
+  m3 <- which(condensed[,8] == 3)
+  m4 <- which(condensed[,8] == 4)
+  m5 <- which(condensed[,8] == 5)
+  m6 <- which(condensed[,8] == 6)
+  
+  res0 <- which(condensed[,6] == 0)
+  res1 <- which(condensed[,6] == 1)
+  
+  r0_fail <- res0[which(res0 %in% r0)]
   r1_fail <- res0[which(res0 %in% r1)]
   r2_fail <- res0[which(res0 %in% r2)]
+  r3_fail <- res0[which(res0 %in% r3)]
+  r0_succ <- res1[which(res1 %in% r0)]
+  r1_succ <- res1[which(res1 %in% r1)]
+  r2_succ <- res1[which(res1 %in% r2)]
+  r3_succ <- res1[which(res1 %in% r3)]
   
+  catch_x <- which(condensed[,12] <= -1)
+  catch0 <- which(condensed[,12] == 0)
+  catchx <- which(condensed[,12] >= 1)
   
+  catch_3 <- which(condensed[,12] == -3)
+  catch_2 <- which(condensed[,12] == -2)
+  catch_1 <- which(condensed[,12] == -1)
+  catch1 <- which(condensed[,12] == 1)
+  catch2 <- which(condensed[,12] == 2)
+  catch3 <- which(condensed[,12] == 3)
+  
+  r0_f <- res0[which(res0 %in% r0)]
+  rx_f <- res0[which(res0 %in% rx)]
+  r0_s <- res1[which(res1 %in% r0)]
+  rx_s <- res1[which(res1 %in% rx)]
+  
+  p0_f <- res0[which(res0 %in% p0)]
+  px_f <- res0[which(res0 %in% px)]
+  p0_s <- res1[which(res1 %in% p0)]
+  px_s <- res1[which(res1 %in% px)]
+  
+  r0_p0 <- r0[which(r0 %in% p0)]
+  rx_p0 <- rx[which(rx %in% p0)]
+  r0_px <- r0[which(r0 %in% px)]
+  rx_px <- rx[which(rx %in% px)]
+  
+  r0_p0_s <- res1[which(res1 %in% r0_p0)]
+  rx_p0_s <- res1[which(res1 %in% rx_p0)]
+  r0_px_s <- res1[which(res1 %in% r0_px)]
+  rx_px_s <- res1[which(res1 %in% rx_px)]
+  r0_p0_f <- res0[which(res0 %in% r0_p0)]
+  rx_p0_f <- res0[which(res0 %in% rx_p0)]
+  r0_px_f <- res0[which(res0 %in% r0_px)]
+  rx_px_f <- res0[which(res0 %in% rx_px)]
 }
 
 
